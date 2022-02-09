ac8c06dd6c8660d442f0407979599f8de225e6b8
dungducphan <brianp.dung@utexas.edu>
2022-02-09 15:54:51 -0600

    Adding scintillation blocks. Recalculate offset and spacing of calorimeter layers.

--- construction.cc
+++ construction.cc
@@ -15,16 +15,24 @@ G4VPhysicalVolume* MyDetectorConstruction::Construct() {
   G4LogicalVolume* logicWorld = new G4LogicalVolume(solidWorld, worldMat, "logicWorld");
   G4VPhysicalVolume* physWorld = new G4PVPlacement(0, G4ThreeVector(0, 0, 0), logicWorld, "physWorld", 0, false, 0, true);
   
-  G4double calorimeterXY = 20 * cm;
-  G4double absorberHalfThickness = 1./2. * cm;
-  G4double scintillatorHalfThickness = 3./2 * cm;
+  G4double calorimeterHalfXY = 20 * cm;
+  G4double absorberHalfThickness = 0.5/2. * cm;
+  G4double scintillatorHalfThickness = 2./2 * cm;
   G4double layerThickness = 2 * (absorberHalfThickness + scintillatorHalfThickness);
+  G4int numLayers = 20;
 
-  G4Material* absorberMat = nist->FindOrBuildMaterial("G4_Fe");
-  G4Box* solidAbsorber = new G4Box("solidAbsorber", calorimeterXY, calorimeterXY, absorberHalfThickness);
+  G4Material* absorberMat = nist->FindOrBuildMaterial("G4_Al");
+  G4Box* solidAbsorber = new G4Box("solidAbsorber", calorimeterHalfXY, calorimeterHalfXY, absorberHalfThickness);
   G4LogicalVolume* logicAbsorber = new G4LogicalVolume(solidAbsorber, absorberMat, "logicAbsorber");
-  for (int i = 0; i < 20; ++i) {
-    new G4PVPlacement(0, G4ThreeVector(0, 0, -1. * i * layerThickness), logicAbsorber, Form("physAbsorber_%02i", i), logicWorld, false, i);
+  for (int i = 0; i < numLayers; ++i) {
+    new G4PVPlacement(0, G4ThreeVector(0, 0,- 2 * scintillatorHalfThickness - absorberHalfThickness - i * layerThickness), logicAbsorber, Form("physAbsorber_%02i", i), logicWorld, false, i);
+  }
+
+  G4Material* scintillatorMat = nist->FindOrBuildMaterial("G4_SODIUM_IODIDE");
+  G4Box* solidScintillator = new G4Box("solidScintillator", calorimeterHalfXY, calorimeterHalfXY, scintillatorHalfThickness);
+  G4LogicalVolume* logicScintillator = new G4LogicalVolume(solidScintillator, scintillatorMat, "logicScintillator");
+  for (int i = 0; i < numLayers; ++i) {
+      new G4PVPlacement(0, G4ThreeVector(0, 0, - scintillatorHalfThickness - i * layerThickness), logicScintillator, Form("physScintillator_%02i", i), logicWorld, false, i);
   }
 
   return physWorld;
